0a5f75d75b32187b6fe6dd2113fabe9c50202ffc
valentincognito <valentin.vannay@gmail.com>
2017-05-17 15:23:02 +0900

    Add Camera control script

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public BowlingBall bowlingBall;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - bowlingBall.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.z <= 1500f){
+			transform.position = bowlingBall.transform.position + offset;
+		}
+	}
+}
