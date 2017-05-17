0839ab8c27f63e0a211f83931805fc2884f137f0
valentincognito <valentin.vannay@gmail.com>
2017-05-17 14:47:11 +0900

    Create bowling ball

---
+++ Assets/Scripts/BowlingBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingBall : MonoBehaviour {
+
+	public float launchSpeed;
+
+	private Rigidbody rigidBody;
+	private AudioSource rollingSound;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		rollingSound = GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+
+	void Launch ()
+	{
+		rigidBody.velocity = new Vector3 (0, 0, launchSpeed);
+		rollingSound.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
