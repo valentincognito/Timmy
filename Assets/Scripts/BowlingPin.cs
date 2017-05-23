cca34f6c4197afb24def903f1d319cb0d89c4a32
valentincognito <valentin.vannay@gmail.com>
2017-05-23 14:30:00 +0900

    Detect if pin is standing

---
+++ Assets/Scripts/BowlingPin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingPin : MonoBehaviour {
+
+	public float standingTreshold;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (name + " - " + IsStanding());
+	}
+
+	public bool IsStanding(){
+		Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+		float tiltInX = Mathf.Abs(rotationInEuler.x);
+		float tiltInZ = Mathf.Abs(rotationInEuler.z);
+
+		if (tiltInX < standingTreshold && tiltInZ < standingTreshold) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
