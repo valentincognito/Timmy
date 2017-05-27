713342ee80ba83db73e2fb9a1daf7b7a4e72d839
Valentin Vannay <valentin@Valentins-MacBook-Air.local>
2017-05-27 10:58:24 +0900

    Create test class for the score master

---
+++ Assets/Editor/ActionMasterTest.cs.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ActionMasterTest {
+
+	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+	[Test]
+	public void T01OneStrikeReturnsEndTurn() {
+		ActionMaster actionMaster = new ActionMaster ();
+		Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+	}
+
+
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster {
+
+	public enum Action
+	{
+		Tidy,
+		Reset,
+		EndTurn,
+		EndGame
+	}
+
+	public Action Bowl(int pins){
+
+		if(pins < 0 || pins > 10){
+			throw new UnityException ("Invalid numbers of pins");
+		}
+
+		if(pins == 10){
+			return Action.EndTurn;
+		}
+
+		throw new UnityException ("Not sure what action to return");
+	}
+}
